43babd96e9785a42cacd7ecb6d80a21bcac06d9f
Richard Downe <richard@casetext.com>
2014-11-24 18:37:59 -0800

    switching branches, so checking in.

--- app.js
+++ app.js
@@ -5,24 +5,37 @@
  */
 
 var es = require('elasticsearch'),
+   optometrist   = require('optometrist'),
    conf          = require('./config'),
    fbutil        = require('./lib/fbutil'),
    PathMonitor   = require('./lib/PathMonitor'),
    SearchQueue   = require('./lib/SearchQueue'),
    logger        = require('./lib/logging').logger;
 
-// connect to ElasticSearch
-var esc = new es.Client({
-  host: conf.ELASTICSEARCH_URL,
-  log: 'error'
-});
-logger.info('Connected to ElasticSearch host %s'.grey, conf.ELASTICSEARCH_URL);
 
-fbutil.auth().then(function() {
-  PathMonitor.process(esc, conf.paths, conf.FB_PATH);
-  SearchQueue.init(esc, conf.FB_REQ, conf.FB_RES, conf.CLEANUP_INTERVAL);
-})
-.catch(function(err) {
-  logger.error('Could not authenticate to Firebase: ' + err);
-  return; 
-});
+var launchService = function(siteConfig) {
+  // connect to ElasticSearch
+  var esc = new es.Client({
+    host: conf.ELASTICSEARCH_URL,
+    log: 'error'
+  });
+
+  logger.info('Connected to ElasticSearch host %s'.grey, conf.ELASTICSEARCH_URL);
+  
+  fbutil.auth().then(function() {
+    PathMonitor.process(esc, conf.paths, conf.FB_PATH);
+    SearchQueue.init(esc, conf.FB_REQ, conf.FB_RES, conf.CLEANUP_INTERVAL);
+  })
+  .catch(function(err) {
+    logger.error('Could not authenticate to Firebase: ' + err);
+    return; 
+  });
+}
+
+if (require.main===module) {
+  var siteConf = require(process.argv[0]);
+  launchService(siteConf);
+}
+else {
+  exports.start = launchService;
+}
