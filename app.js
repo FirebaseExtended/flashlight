364e3346c28d2fd533db44af4fd8ed3c86a1d51b
Richard Downe <richard@casetext.com>
2014-11-24 18:07:43 -0800

    replaced all calls to console.log with calls to bunyan.

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ var es = require('elasticsearch'),
    conf          = require('./config'),
    fbutil        = require('./lib/fbutil'),
    PathMonitor   = require('./lib/PathMonitor'),
-   SearchQueue   = require('./lib/SearchQueue');
+   SearchQueue   = require('./lib/SearchQueue'),
+   logger        = require('./lib/logging').logger;
 
 // connect to ElasticSearch
 var esc = new es.Client({
   host: conf.ELASTICSEARCH_URL,
   log: 'error'
 });
-console.log('Connected to ElasticSearch host %s'.grey, conf.ELASTICSEARCH_URL);
+logger.info('Connected to ElasticSearch host %s'.grey, conf.ELASTICSEARCH_URL);
 
 fbutil.auth().then(function() {
   PathMonitor.process(esc, conf.paths, conf.FB_PATH);
   SearchQueue.init(esc, conf.FB_REQ, conf.FB_RES, conf.CLEANUP_INTERVAL);
 })
 .catch(function(err) {
-  console.log(err);
+  logger.error('Could not authenticate to Firebase: ' + err);
   return; 
 });
